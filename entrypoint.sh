585c00f6b21b7191d29beffdf281bf6958386a3b
bxuxa <bxuxa@cn.ibm.com>
2019-04-19 15:57:26 +0800

    special handling for xcatconfig -d

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,16 @@ if [[ -d "/xcatdata.NEEDINIT"  ]]; then
     echo "initializing xCAT ..."
     rsync -a /xcatdata.NEEDINIT/ /xcatdata
     mv /xcatdata.NEEDINIT /xcatdata.orig
-    xcatconfig -i
+    xcatconfig -d
 
     #echo "initializing networks table..."
-    #tabprune networks -a
-    #makenetworks
+    xcatconfig -i
+    XCATBYPASS=1 tabdump site|grep domain || XCATBYPASS=1 chtab key=domain site.value=example.com
+
+    echo "create symbol link for /root/.xcat..."
+    rsync -a /root/.xcat/* /xcatdata/.xcat
+    rm -rf /root/.xcat/
+    ln -sf -t /root /xcatdata/.xcat
 
     echo "initializing loop devices..."
     # workaround for no loop device could be used by copycds
@@ -28,7 +33,7 @@ cat /etc/motd
 HOSTIPS=$(ip -o -4 addr show up|grep -v "\<lo\>"|xargs -I{} expr {} : ".*inet \([0-9.]*\).*")
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo "welcome to Dockerized xCAT, please login with"
-[[ -n "$HOSTIPS"  ]] && for i in $HOSTIPS; do echo "   ssh root@$i   "; done && echo "The initial password is \"cluster\""
+[[ -n "$HOSTIPS"  ]] && for i in $HOSTIPS; do echo "   ssh root@$i -p 2200  "; done && echo "The initial password is \"cluster\""
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
 
